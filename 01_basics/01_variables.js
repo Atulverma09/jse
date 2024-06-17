8bf09ce4cf8fdd72770005d880823ee05f43456b
Atulverma09 <143073449+Atulverma09@users.noreply.github.com>
2024-06-17 09:09:14 +0000

    console config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+ const atul = 2
+let acountid = 123545
+var acountemail = "atulverma2099@gmail.com"
+ const acountpassword = "12345"
+
+
+console.table([atul , acountid,acountemail,acountpassword]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+let age =19;
+let name = "atulverma"
+let ismarriage = false;
+let is;
+let sign = null;
+
+
+console.table([typeof ismarriage ,is, age , sign]);
