c2caa6c14982d823ca22caaff44a38c768cb935e
Atulverma09 <143073449+Atulverma09@users.noreply.github.com>
2024-06-21 07:44:20 +0000

    consol

--- 01_basics/04_strings.js
+++ 01_basics/04_strings.js
@@ -1,6 +1,39 @@
-//sring
 
 let string= "Atul verma";
 let rcount = 40;
 
 console.log(string + rcount);
+
+console.log(`hello my name is ${string} and rcount is ${rcount}`);
+
+const gameover = new String('thisisbusiness');
+console.log(gameover[0]);
+console.log(gameover.__proto___);
+console.log(gameover.toUpperCase());
+console.log(gameover.charAt(2));
+
+const news = gameover.substring(0,4);
+console.log(news);
+const another = gameover.slice(-14,9);
+//in slice we can strart with negative also 
+console.log(another);
+
+
+// trim left right ki places ko hata dega
+
+
+let trimkaro = ("  thisis business   ");
+console.log(trimkaro.trim());
+console.log(trimkaro);
+console.log(trimkaro.replace("thisis " ,"sunder"));
+console.log(trimkaro.includes("sunder"));
+console.log(trimkaro.split("  "));
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/05_nummath.js
@@ -0,0 +1,17 @@
+const score = 1100;
+const balance = new Number(100);
+console.log(balance);
+console.log(score);
+console.log(score.toString().length);
+console.log(balance.toString().length);
+
+const other = 1123.8989;
+console.log(other.toPrecision(3));
+
+
+//math
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,3,5,20,7,6,10,9));
