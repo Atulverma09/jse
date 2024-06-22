3ac7fcfbae43329fa8b64f3e47e13fed2cecdbd4
Atulverma09 <143073449+Atulverma09@users.noreply.github.com>
2024-06-22 13:17:07 +0000

    consol

--- 02_objects/4od.js
+++ 02_objects/4od.js
@@ -1,2 +1,28 @@
 //object de-structure and json
+const billu = {
+    name:"atul",
+    age: 19,
+    classowner:"ajoo"
+}
+const {classowner : myclass} = billu;
+console.log(myclass);
+
+
+let {age : umar} = billu;
+console.log(umar);
+
+
+{
+    "name":"av",
+    "age":"19"
+}
+
+[
+    {},
+    {},
+    {},
+    {}
+]
+
+
 
