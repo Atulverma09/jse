0a78dd84da706224a6e9918b55405f1f5f8fde97
Atulverma09 <143073449+Atulverma09@users.noreply.github.com>
2024-06-22 08:42:49 +0000

    console

---
+++ 01_basics/06_array.js
@@ -0,0 +1,26 @@
+const arr = [1,2,3,4,5,6,6,7];
+
+console.log(arr);
+
+console.log(arr.length);
+console.log(arr.toLocaleString());
+
+console.log(arr.slice(1,3));
+console.log(arr);
+
+console.log(arr.splice(1,3));
+console.log(arr);
+
+arr.push(17);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.unshift(17);
+console.log(arr);
+
+arr.shift(17);
+console.log(arr);
+
+console.log(arr.includes(5));
