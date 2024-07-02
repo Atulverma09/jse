12f0235053015b076a453c9a13d67d93681f5f80
Atulverma09 <143073449+Atulverma09@users.noreply.github.com>
2024-07-02 11:48:45 +0000

    console

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,3 +6,17 @@ let sign = null;
 
 
 console.table([typeof ismarriage ,is, age , sign]);
+
+//primitive datatypes(value allocate) 
+//string boolean number null undefined  symbol BigInt
+
+const BigNumber = 11111111111111111111n;
+
+//reference type (non-primitive )
+//objects , arrays, functions
+// return typr is always object
+
+
+
+//javascript is a dynamically typed language because no 
+//no need to define it
