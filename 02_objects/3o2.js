7587527ddb07b9bbd50164cbfd9e061a1e8656b0
Atulverma09 <143073449+Atulverma09@users.noreply.github.com>
2024-07-03 07:57:17 +0000

    console

--- 02_objects/3o2.js
+++ 02_objects/3o2.js
@@ -1,13 +1,41 @@
+// ye ek singleton object hai 
+//const  tinder = new Object()
+
+
+const tinder ={}
+//ye ek non-singleton object hai
+
+tinder.id ="at8u";
+tinder.mail = "aaaa@gmail";
+tinder.islogin = false;
+console.log(tinder)
+
+
+const rUser = {
+    name: "jilo",
+    kaam :{
+        afcat : "Audi",
+        gafcat: "jpo",
+        bag :{
+            av: "as"
+        }
+    }
+}
+console.log(rUser.kaam.bag)
+
+
 //object assign karna 
 
 const obj1 = { 1:"a" , 'b':2,'c':3}
 const obj2 = {'d':4, 'e':5, 'f':6}
-const obj3 = {'g':5, 'h':8, 'i':9}
+const obj3 = {'g':4, 'h':8, 'i':9}
 
 const obj4 = Object.assign({}, obj1, obj2, obj3);
+//this is good to apply{} in it give perfect value
 console.log(obj4);
 
 const obj5 = { ...obj1, ...obj2, ...obj3};
+//it spreads value spread 
 console.log(obj5);
 
 
@@ -18,7 +46,7 @@ const will ={
     mail: "add.com"
 }
 console.log(will);
-console.log(Object.keys(will));
-console.log(Object.values(will));
-console.log(Object.entries(will))
+console.log(Object.keys(will));  //provide key  using array
+console.log(Object.values(will));// provide values using array
+console.log(Object.entries(will));//provides entries both in array
 console.log(will.hasOwnProperty('id'))
--- 02_objects/4od.js
+++ 02_objects/4od.js
@@ -4,6 +4,8 @@ const billu = {
     age: 19,
     classowner:"ajoo"
 }
+// isme apne ko direct value nikal sakte hai
+// aur apan ne classowner ko myclass banke value destructure kar di
 const {classowner : myclass} = billu;
 console.log(myclass);
 
@@ -11,10 +13,12 @@ console.log(myclass);
 let {age : umar} = billu;
 console.log(umar);
 
+//json 
+// json formatter
 
 {
     "name":"av",
-    "age":"19"
+    "age": "19"
 }
 
 [
