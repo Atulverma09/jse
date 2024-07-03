65b63b55102b6b65a65db5894956a79df17af648
Atulverma09 <143073449+Atulverma09@users.noreply.github.com>
2024-07-03 08:53:03 +0000

    console

---
+++ 03_functions/1fun.js
@@ -0,0 +1,32 @@
+function myname(){
+
+    console.log("atul verma")
+    console.log("atul verma hth")
+}
+
+ myname()
+
+
+ //add two numbers
+
+ function add(a,b){  //(a,b ) are parameters
+
+    return a+b
+ }
+
+ console.log(add(2,3))  //(2,3)  are arguments just names
+
+
+
+ function userbhai(username){
+    return `${username} just logged in `
+ }
+//if you not give anything than it will provide undefined
+ //console.log(userbhai());
+
+ console.log(userbhai("avbnn"));
+
+
+
+ //default value dene ke liye simplii yahi pe equal kar ke value de de
+// function userbhai(username ="samm"){
