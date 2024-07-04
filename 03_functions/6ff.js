e24f340aa871c0944ce29bab352d0ae8924de51c
Atulverma09 <143073449+Atulverma09@users.noreply.github.com>
2024-07-04 11:27:42 +0000

    console

---
+++ 03_functions/6ff.js
@@ -0,0 +1,16 @@
+//How does javascript execute code + call stack
+
+//global execution context
+//function execution context
+//eval  execution context   it is property of (gec)
+
+//two phases 
+//1. memory creation phase = bas memory allocate kar do
+//2. execution phase= 
+
+
+//global execution == this 
+//memory phase
+//execution phase
+
+//implement by call stack means we lifo
