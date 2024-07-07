46a6536ea3007cc2137d1842993fb7aa5b1a4172
Atulverma09 <143073449+Atulverma09@users.noreply.github.com>
2024-07-07 13:39:38 +0000

    console

---
+++ 06_dom/00.js
@@ -0,0 +1,5 @@
+//Dom is a document object model 
+
+//respoonsible for the data representation of the //objects that comprise the structure and content of a document on the web.
+
+//document.getElementById
