5ac30f85a3c682fa6c62da849c3e726f0f6c1091
Atulverma09 <143073449+Atulverma09@users.noreply.github.com>
2024-07-10 07:09:55 +0000

    console

--- 06_dom/00.js
+++ 06_dom/00.js
@@ -3,3 +3,11 @@
 //respoonsible for the data representation of the //objects that comprise the structure and content of a document on the web.
 
 //document.getElementById
+
+const addtext = document.createTextNode("chai aur code")
+
+undefined
+div.appendChild(addtext)
+"chai aur code"
+document.body.appendChild(div)
+<div>​chai aur code​</div>​
